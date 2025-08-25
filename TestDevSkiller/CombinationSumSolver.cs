82c879ad6eb7586e07b7a3dd78892c01bd3734cb
mukhr <ma-rinat@mail.com>
2025-08-25 19:27:22 -0400

    #2 Remove old tasks.

--- TestDevSkiller/CombinationSumSolver.cs
+++ TestDevSkiller/CombinationSumSolver.cs
@@ -8,25 +8,27 @@ namespace TestDevSkiller
         public List<List<int>> CombinationSum(int[] candidates, int target)
         {
             var result = new List<List<int>>();
-            Backtrack(candidates, target, 0, new List<int>(), result);
+            Backtrack(candidates, target, new List<int>(), result);
             return result;
         }
 
-        private void Backtrack(int[] candidates, int target, int start, List<int> current, List<List<int>> result)
+        private void Backtrack(int[] candidates, int target, List<int> current, List<List<int>> result)
         {
-            if (target == 0)
+            if(target < 0)
+            {
+                return;
+            }
+
+            if(target == 0)
             {
                 result.Add(new List<int>(current));
                 return;
             }
 
-            if (target < 0)
-                return;
-
-            for (var i = start; i < candidates.Length; i++)
+            foreach(int candidate in candidates)
             {
-                current.Add(candidates[i]);
-                Backtrack(candidates, target - candidates[i], i, current, result);
+                current.Add(candidate);
+                Backtrack(candidates, target - candidate, current, result);
                 current.RemoveAt(current.Count - 1);
             }
         }
